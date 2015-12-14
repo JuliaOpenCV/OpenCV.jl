1b97dfb81970fe08b20823cae83581ca9dad89cf
Ryuichi YAMAMOTO <zryuichi@gmail.com>
2015-12-15 01:06:03 +0900

    Widen argment types for imwrite so that it can acepst cv2.UMat as well as cv2.Mat

--- src/imgcodecs.jl
+++ src/imgcodecs.jl
@@ -2,8 +2,7 @@ function imread(name::AbstractString, flag=IMREAD_UNCHANGED)
     Mat(@cxx cv::imread(pointer(name), flag))
 end
 
-imwrite(filename::AbstractString, img::Mat) =
+imwrite(filename::AbstractString, img::AbstractCvMat) =
     @cxx cv::imwrite(pointer(filename), handle(img))
-function imwrite(filename::AbstractString, img::Mat, params)
+imwrite(filename::AbstractString, img::AbstractCvMat, params) =
     @cxx cv::imwrite(pointer(filename), handle(img), params)
-end
