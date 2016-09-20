f77f5982cca8fe9459b0f1482b462496f46d533e
Ryuichi YAMAMOTO <zryuichi@gmail.com>
2016-09-20 18:36:39 +0900

    tweak

--- src/OpenCV.jl
+++ src/OpenCV.jl
@@ -3,9 +3,10 @@ module OpenCV
 using Reexport
 
 @reexport using CVCore
+@reexport using CVCalib3d
 @reexport using CVHighGUI
+@reexport using CVVideoIO
 @reexport using CVImgProc
 @reexport using CVImgCodecs
-@reexport using CVVideoIO
 
 end # module OpenCV
