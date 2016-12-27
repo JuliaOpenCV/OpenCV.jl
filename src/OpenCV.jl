876ca28880bdfe7f70684f2ecc7b72a95ab646d2
Ryuichi YAMAMOTO <zryuichi@gmail.com>
2016-12-27 12:41:24 +0900

    Add CVVideo

--- src/OpenCV.jl
+++ src/OpenCV.jl
@@ -80,6 +80,7 @@ using Reexport
 @reexport using CVCore
 @reexport using CVCalib3d
 @reexport using CVHighGUI
+@reexport using CVVideo
 @reexport using CVVideoIO
 @reexport using CVImgProc
 @reexport using CVImgCodecs
